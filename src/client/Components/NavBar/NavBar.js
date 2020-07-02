36bb186446872adade13fa45d1b51148b2ff0049
Angel Sirodey <xirodey@gmail.com>
2020-07-02 14:32:28 +0200

    fix reload

--- src/client/Components/NavBar/NavBar.js
+++ src/client/Components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import { useMovieContext } from '../../Store/movieSearchContext';
 import Login from '../Login';
@@ -11,10 +11,12 @@ const NavBar = ({ isAuth, setIsAuth }) => {
   const { searchError } = useMovieContext();
   const notMovies = searchError === 'Movie not found!' ? 'Movie not found!' : null;
   const [openLogin, setOpenLogin] = useState(false);
-  
-  const refreshPage = () => (
-    window.location.reload(false)
-  );
+  let history = useHistory();
+
+  const reload = () => {
+    history.push('/favorites');
+    window.location.reload(false);
+  };
 
   return (
     <Fragment>
@@ -24,7 +26,7 @@ const NavBar = ({ isAuth, setIsAuth }) => {
           <div className="NavBar__list">
           <Link to="/" className="favorites__link"><div className="NavBar__list--text">Home</div></Link>
             {isAuth ? 
-            <Link to="/favorites" onClick={refreshPage} className="favorites__link">
+            <Link to="/favorites" onClick={reload} className="favorites__link">
               <div  className="NavBar__list--text">Favorites</div>
             </Link> :
              <div onClick={() => setOpenLogin(true)} className="NavBar__list--text">Login</div>}
