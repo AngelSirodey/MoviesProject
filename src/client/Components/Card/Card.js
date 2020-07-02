4fc8eb34f040525b4d38e96b6de31b1731aab591
Angel Sirodey <xirodey@gmail.com>
2020-07-02 22:19:35 +0200

    fix save in favorites same movie

--- src/client/Components/Card/Card.js
+++ src/client/Components/Card/Card.js
@@ -7,9 +7,17 @@ import { useMovieContext } from '../../Store/movieSearchContext';
 import './Card.css';
 
 const Card = ({ movie, fromFavorite, handleDeleteFavorites }) => {
-  const { saveFavorites } = useMovieContext();
+  const { saveFavorites, userFavorites } = useMovieContext();
   const { Poster, Title, imdbID } = movie;
   const detailsURL = `/details?imdbID=${imdbID}`;
+
+  const saveMovieInFavorites = (movie) => {
+    const movieAlreadyExist = userFavorites.find(movieId => movieId.imdbID === movie.imdbID);
+    if (movieAlreadyExist) {
+      return null;
+    };
+    return saveFavorites(movie);
+  };
 
    return (
       <div className="Card__container">
@@ -18,7 +26,7 @@ const Card = ({ movie, fromFavorite, handleDeleteFavorites }) => {
           <p className="Card__title">{Title}</p>
           <div className="Card__links">
             {!fromFavorite ?
-              <p onClick={() => saveFavorites(movie)} >Add to favotires</p> :
+              <p onClick={() => saveMovieInFavorites(movie)} >Add to favotires</p> :
               <p onClick={() => handleDeleteFavorites(movie)} className="Card__favorite--delete" >Remove</p>}
            <Link to={detailsURL}> More details -></Link>
           </div>
